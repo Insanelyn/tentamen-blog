98099b8d85cae9ae51b270e939d041307ad7ac73
Evelyn Jepsen <evelynjepsen@gmail.com>
2018-04-20 13:40:07 +0100

    soon

--- contents/g/app.js
+++ contents/g/app.js
@@ -1,1 +1,42 @@
-// JavaScript för att implementera kraven A-D.
+$(() => {
+    let posts = [];
+    let bloggpage = [];
+
+
+
+
+    $.getJSON( "https://jsonplaceholder.typicode.com/posts", function( data ) {
+        posts = data;
+
+        for (let post of posts) {
+            post.Likes = Math.floor((Math.random() * 100));
+        }
+
+        renderBlogPosts();
+
+    });
+
+    function renderBlogPosts() {
+        let $bloggPosts = $("#bloggPosts");
+        $bloggPosts.empty();
+        for (let post of posts) {
+            let $post = $(`<div class="product">
+                <div>${post.id}</div>
+                <div>${post.title}</div>
+                <div>${post.body}"></div>
+                <div>${post.Likes} Likes</div>
+                <button id="commentButton">Comment</button>
+            </div>`);
+
+            $post.appendTo($bloggPosts);
+        }
+    }
+    $('#commentButton').click(function(){
+//display fuking comments here..
+    });
+
+
+
+});
+
+
